d55420a85b9698eda321eca9f22ff9ac4cd91720
krlargo <krlargo@ucdavis.edu>
2017-12-16 23:12:41 -0800

    Implement urlSession.didFinishDownloadingTo()

--- HalfTunes/SearchVC+URLSessionDelegates.swift
+++ HalfTunes/SearchVC+URLSessionDelegates.swift
@@ -10,6 +10,30 @@ import Foundation
 
 extension SearchViewController: URLSessionDownloadDelegate {
   func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-    print("Finished downloading to \(location)");
+    /// Remove download from activeDownloads
+    guard let sourceURL = downloadTask.originalRequest?.url else { return }
+    let download = downloadService.activeDownloads[sourceURL]
+    downloadService.activeDownloads[sourceURL] = nil
+    
+    /// Get destinationURL by appending filename.ext to the path of the app’s Documents directory
+    let destinationURL = localFilePath(for: sourceURL)
+    print(destinationURL)
+
+    /// Remove localFileath of download
+    let fileManager = FileManager.default
+    try? fileManager.removeItem(at: destinationURL) /// Remove anything currently in destinationURL
+    do { /// Copy download from location to destination
+      try fileManager.copyItem(at: location, to: destinationURL)
+      download?.track.downloaded = true
+    } catch let error {
+      print("Could not copy file to disk: \(error.localizedDescription)")
+    }
+    
+    /// Reload the index of the downloaded track
+    if let index = download?.track.index {
+      DispatchQueue.main.async {
+        self.tableView.reloadRows(at: [IndexPath(row: index, section: 0)], with: .none)
+      }
+    }
   }
 }
